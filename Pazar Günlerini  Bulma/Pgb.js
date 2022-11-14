98aa39c8cfb7d0f5138c940f9908fddc1379f543
Banu Seyhan <banuaskahraman@gmail.com>
2022-11-14 20:36:37 +0300

    pazar günü eksik

---
+++ Pazar Günlerini  Bulma/Pgb.js
@@ -0,0 +1,14 @@
+function getSundayOfCurrentWeek() {
+  const today = new Date();
+  const first = today.getDate() - today.getDay() + 1;
+  const last = first + 6;
+
+  const monday = new Date(today.setDate(first));
+  console.log(monday); // pazartesi hangi günr denk geliyor??
+
+  const sunday = new Date(today.setDate(last));
+
+  return sunday;
+}
+
+console.log(getSundayOfCurrentWeek());
