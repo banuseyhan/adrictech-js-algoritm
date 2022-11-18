d601d285e21179e721b99dd52241239fe31bfbbc
Banu Seyhan <banuaskahraman@gmail.com>
2022-11-18 11:53:43 +0300

    fixed

--- Pazar Günlerini  Bulma/index.js
+++ Pazar Günlerini  Bulma/index.js
@@ -1,67 +1,71 @@
 function findSundays() {
   const sundays = [];
 
-  const WEEK_DAYS = 7;
-  const REGULAR_DAYS_IN_YEAR = {
-    '1': 31,
-    '2': 28,
-    '3': 31,
-    '4': 30,
-    '5': 31,
-    '6': 30,
-    '7': 31,
-    '8': 31,
-    '9': 30,
-    '10': 31,
-    '11': 30,
-    '12': 31,
+  // haftanın günleri
+  const wd = 7;
+
+  //sabit yıl
+  const sy = {
+    1: 31,
+    2: 28,
+    3: 31,
+    4: 30,
+    5: 31,
+    6: 30,
+    7: 31,
+    8: 31,
+    9: 30,
+    10: 31,
+    11: 30,
+    12: 31,
+  };
+  //artık yıl
+  const ay = {
+    1: 31,
+    2: 29,
+    3: 31,
+    4: 30,
+    5: 31,
+    6: 30,
+    7: 31,
+    8: 31,
+    9: 30,
+    10: 31,
+    11: 30,
+    12: 31,
   };
 
-  const LEAP_YEAR = {
-    '1': 31,
-    '2': 29,
-    '3': 31,
-    '4': 30,
-    '5': 31,
-    '6': 30,
-    '7': 31,
-    '8': 31,
-    '9': 30,
-    '10': 31,
-    '11': 30,
-    '12': 31,
-  }
-
   let year = 1900;
-  // this is because 7.1.1900 is sunday
+  // 7.1.1900 pazar günü düşünürsek
   let sundayDate = 7;
 
   while (year <= 2000) {
-    for (const key in REGULAR_DAYS_IN_YEAR) {
+    for (const key in sy) {
       if (year % 4 === 0 && year !== 1900) {
-        // februaries are 29 days
-        while (sundayDate < LEAP_YEAR[key]) {
-          sundayDate += WEEK_DAYS;
+        // şubat 29 ise
+        while (sundayDate < ay[key]) {
+          sundayDate += wd;
         }
 
-        sundayDate -= LEAP_YEAR[key];
+        sundayDate -= ay[key];
       } else {
-        // februaries are 28 days
-        while (sundayDate < REGULAR_DAYS_IN_YEAR[key]) {
-          sundayDate += WEEK_DAYS;
+        // şubat 28 ise
+        while (sundayDate < sy[key]) {
+          sundayDate += wd;
         }
 
-        sundayDate -= REGULAR_DAYS_IN_YEAR[key];
+        sundayDate -= sy[key];
       }
-      
-   
+
+      // +key + 1 doğru ayı çağırma
+
       if (sundayDate === 1 && +key < 12) {
         sundays.push(`1/${+key + 1}/${year}`);
       } else if (sundayDate === 1 && +key === 12) {
         sundays.push(`1/1/${year + 1}`);
       }
     }
-    
+
     year++;
   }
 
